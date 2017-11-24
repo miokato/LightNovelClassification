463b0451d3f65fe44b226d022384955899741b26
miokato <miokato@me.com>
2017-11-24 23:05:13 +0900

    add view

--- classification/views.py
+++ classification/views.py
@@ -2,18 +2,28 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views.generic import TemplateView
 
+from classification.tools.parser import MessageManager, CabochaParser
+from classification.tools.predict import Predictor
+
 
 class SampleView(TemplateView):
-    print('hello')
     template_name = 'classification/index.html'
 
     def get(self, request, *args, **kwargs):
+        return render(request, 'classification/index.html')
 
+    def post(self, request):
+        manager = MessageManager(CabochaParser())
+        predictor = Predictor()
+
+        text = request.POST.get('message')
+        mes = manager.extract_message(text)
+        words = mes.bags
+        prediction = predictor.predict(words)
+        print(prediction)
         ctx = {
-            'neko': 'mochi',
-            'nuko': 'inu',
+            'answer': prediction
         }
-
         return self.render_to_response(context=ctx)
 
 
